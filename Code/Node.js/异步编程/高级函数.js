854da6d85302fd4674b62795408c496950247e52
zengshine <shineview1992@outlook.com>
2019-05-12 17:54:23 +0800

    Daily Async

---
+++ Code/Node.js/异步编程/高级函数.js
@@ -0,0 +1,1 @@
+var isType = function (type) { return function (obj) { return toString.call(obj) == '[object ' + type + ']'; }; }; var isString = isType('String'); var isFunction = isType('Function');
